5927c6304e823ac234dab93b0e6c77dfbed14e30
Afif <afiff1283@gmail.com>
2026-09-16 20:48:10 +0700

    feat: add python file

---
+++ python/bioskop.py
@@ -0,0 +1,35 @@
+class Bioskop:
+    __kode = ""
+    __lokasi = ""
+    __jaringan = ""
+    __kapasitas = 0
+
+    def __init__(self, kode:str, lokasi:str, jaringan:str, kapasitas:int):
+        self.__kode = kode
+        self.__lokasi = lokasi
+        self.__jaringan = jaringan
+        self.__kapasitas = kapasitas
+
+    def getKode(self):
+        return self.__kode
+
+    def getLokasi(self):
+        return self.__lokasi
+
+    def getJaringan(self):
+        return self.__jaringan
+
+    def getKapasitas(self):
+        return self.__kapasitas
+
+    def setKode(self, kode:str):
+        self.__kode = kode
+
+    def setLokasi(self, lokasi:str):
+        self.__lokasi = lokasi
+
+    def setJaringan(self, jaringan:str):
+        self.__jaringan = jaringan
+
+    def setKapasitas(self, kapasitas:int):
+        self.__kapasitas = kapasitas
