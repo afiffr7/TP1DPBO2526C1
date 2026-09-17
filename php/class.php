3a29b919e409bc147b4544a62a399391d579a307
Afif <afiff1283@gmail.com>
2026-09-17 14:50:31 +0700

    fix: rename class

--- php/class.php
+++ php/class.php
@@ -1,12 +1,12 @@
 <?php
-    class Bioskop
+    class Studio
     {
-        private $kode;
-        private $lokasi;
-        private $jaringan;
-        private $kapasitas;
+        private string $kode;
+        private string $lokasi;
+        private string $jaringan;
+        private int $kapasitas;
 
-        public function __construct($kode, $lokasi, $jaringan, $kapasitas)
+        public function __construct(string $kode, string $lokasi, string $jaringan, int $kapasitas)
         {
             $this->kode = $kode;
             $this->lokasi = $lokasi;
@@ -14,19 +14,19 @@
             $this->kapasitas = max($kapasitas, 5);
         }
 
-        public function setKode($kode)
+        public function setKode(string $kode)
         {
             $this->kode = $kode;
         }
-        public function setLokasi($lokasi)
+        public function setLokasi(string $lokasi)
         {
             $this->lokasi = $lokasi;
         }
-        public function setJaringan($jaringan)
+        public function setJaringan(string $jaringan)
         {
             $this->jaringan = $jaringan;
         }
-        public function setKapasitas($kapasitas)
+        public function setKapasitas(int $kapasitas)
         {
             $this->kapasitas = max($kapasitas, 5);
         }
